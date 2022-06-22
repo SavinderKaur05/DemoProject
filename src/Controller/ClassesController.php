c8f5fc2ad6f6373104adc198de4f062424ca5529
SavinderKaur05 <savinderk@codebrigade.in>
2022-06-22 22:17:31 +0530

    .Added Code for Classes

--- src/Controller/ClassesController.php
+++ src/Controller/ClassesController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Classes;
+use App\Form\ClassFormType;
+use App\Repository\ClassesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClassesController extends AbstractController
 {
-    #[Route('/classes', name: 'app_classes')]
-    public function index(): Response
+    
+    
+    #[Route('/create', name: 'Create_Class')]
+    public function create(Request $request): Response
     {
-        return $this->render('classes/AddClass.html.twig', [
-            'controller_name' => 'ClassesController',
-        ]);
+       // $em = EntityManagerInterface();
+       $class = new Classes();
+       $form = $this->createForm(ClassFormType::class,$class);
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid())
+      {
+        $newClass= $form->getData();
+        dd($newClass);
+        exit;
+      }
+
+       return $this->render('classes/AddClass.html.twig',[
+        'form' => $form->createView()
+       ]);
     }
 }
